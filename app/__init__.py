5c7bf63780c5e867be11a3bda8a3c4c79d30cb0e
GillesChristian <christiank.kamgaing@gmail.com>
2024-09-09 13:01:05 +0100

    cors dependencies add

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.domain.exceptions import HTTPException
 
 
 def create_app():
     app = FastAPI()
-
+# Configure CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],  # Replace with your frontend URL
+        allow_credentials=True,
+        allow_methods=["GET","POST","PUT","DELETE"],  # Allows all methods (GET, POST, etc.)
+        allow_headers=["*"],  # Allows all headers
+    )
     @app.exception_handler(HTTPException)
     async def exception_handler(_, exception: HTTPException):
         return JSONResponse(
--- app/test/test_api.py
+++ app/test/test_api.py
@@ -1,14 +1,14 @@
 from starlette.testclient import TestClient
 
-from app import create_app
+from main import create_app
 
 app = create_app()
 
 client = TestClient(app)
 
 def test_get_students():
-    response = client.get("api/students")
-    print(dir(response))
+    response = client.get("/api/students")
+    print(response.status_code)
     assert response.status_code == 200
-
+    
 test_get_students()
